be4a84f1f74d151e6d7b498f43de5d2f304cda43
Subodh-Bny <adhikarisubodh77@gmail.com>
2024-12-09 10:23:42 +0545

    Complete patient registeration

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 declare type SearchParamProps = {
-  params: { [key: string]: string };
+  params: Promise<{ [key: string]: string }>;
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
